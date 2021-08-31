936ce444c3412106725d774857777253860cb7a9
Nohely <ngedeon14@yahoo.com>
2021-08-31 14:34:59 +0200

    Commit and Push

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class Demo
+    {
+
+        public Demo (string Word, string Number )
+        {
+            string word = Word;
+            string number = Number;
+        
+        }
+
+        public string Word { get; set; }
+        public string Numbers { get; set; }
+    }
+}
